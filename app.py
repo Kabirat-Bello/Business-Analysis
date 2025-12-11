3040ce85d5e57a3d322571dddb27c789768f485b
Kabirat-Bello <Kabiratbello7@gmail.com>
2025-12-11 05:18:10 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly_express as px
+import plotly.express as px
 
 # reading the csv file.
 df = pd.read_csv("Business_sales_EDA.csv", sep =";", index_col= "Product ID")
